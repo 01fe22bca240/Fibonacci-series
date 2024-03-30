e6733d1e8ab6ca9ff64735acf0009fef5a62e94d
Kapil G Ratan <01fe22bca240@kletech.ac.in>
2024-03-30 20:19:47 +0530

    Pushing main method for fibonacci series

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[])
+    {
+        int N = 10;
+        FibonacciSeries obj = new FibonacciSeries();
+        obj.fibonacci(N);
+    }
+}
